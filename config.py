dbfff94a271316befeaaa97da26d1c21fa29228e
vishalpandeynkp1 <nishathakur.nkp@gmail.com>
2024-09-20 22:29:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BABY-MUSIC/BABYXMUSIC",
+    "https://github.com/vishalpandeynkp1/BABY-JANU-LAUDA-MUSIC-REPO",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
